33f6c541408d600170ef23651ee70daa7c0f6d4b
sidneybispo <protecney@gmail.com>
2023-12-27 13:24:20 -0300

    commit inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LayoutComponent, children: [
+     { path: 'home', component: HomeComponent }
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+  cadastrando: boolean;
+  mensagemSucesso: string;
+  errors: String[];
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+
+  onSubmit(){
+    this.router.navigate(['/home'])
+  }
+
+  preparaCadastrar(event){
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelaCadastro(){
+    this.cadastrando = false;
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService
+        .salvar(usuario)
+        .subscribe( response => {
+            this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login."
+        }, errorResponse  => {
+          this.mensagemSucesso = null;
+          this.errors = errorResponse.error.errors;
+        })
+  }
+
+}
